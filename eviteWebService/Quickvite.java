0a9b5337550307fe00adec9b64910af8dcd73fe6
Todd Hodes <todd@locationlabs.com>
2008-01-17 03:08:38 +0000

    add code to show the content of retrievedUpcomingInvites
    git-svn-id: svn+ssh://svn/wm/users/todd@79206 33ded623-5efc-0310-b085-d7f78f97f785

--- eviteWebService/Quickvite.java
+++ eviteWebService/Quickvite.java
@@ -25,13 +25,51 @@ public class Quickvite {
             QVCredentials creds = new QVCredentials("wavemarket", "t64Hadre");
             String userId = port.login(creds, login, pass, "5105551212");
 
+            System.out.println("userId is: " + userId);
+
             ArrayOfQVInviteDecorator qvArr = 
               port.retrieveUpcomingInvites(creds, userId, new GregorianCalendar());
+
+            QVInviteDecorator[] invites = qvArr.getQVInviteDecorator();
+            if (invites != null && invites.length > 0) {
+                System.out.println(invites.length + " invites returned.");
+                for (int i=0; i < invites.length; i++) {
+                    QVInviteDecorator inv = invites[i];
+
+                    QVDate end = inv.getEventEndTime();
+                    QVDate start = inv.getEventStartTime();
+                    System.out.println("title is: " + inv.getEventTitle());
+                    System.out.println("hostId is: " + inv.getEventHostUserId());
+                    System.out.println("starts: " + formatQVDate(start));
+                    System.out.println("ends: " + formatQVDate(end));
+                    
+                    QVInvite qv = inv.getInvite();
+                    System.out.println("eventId: " + qv.getEventId());
+                    System.out.println("inviteId: " + qv.getInviteId());
+                    System.out.println("name: " + qv.getName());
+                    System.out.println("email: " + qv.getEmail());
+                    System.out.println("comments: " + qv.getComments());
+                    System.out.println("response: " + qv.getResponse().getValue());
+                    
+                }
+
+            } else {
+                System.out.println("No invites returned.");
+            }
 
 	} catch (Throwable t) {
 	    t.printStackTrace();
 	}
 	
+    }
+
+    public static String formatQVDate(QVDate d) {
+        String ret = "";
+
+        ret += d.getMonth() + "/"  + d.getDay() + "/"  + d.getYear();
+        ret += " " + d.getHours() + ":" + d.getMinutes();
+
+        return ret;
     }
 
 }
