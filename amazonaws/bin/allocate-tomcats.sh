4ec3f426faee506625c5092465cbfead1000d05b
Todd Hodes <todd@locationlabs.com>
2009-12-24 01:41:00 +0000

    bug:26216
    - finish the tomcat + load-balance allocation script
    - add tomcat + load-balance terminate script
    
    
    
    git-svn-id: svn+ssh://svn/wm/users/todd@115460 33ded623-5efc-0310-b085-d7f78f97f785

--- amazonaws/bin/allocate-tomcats.sh
+++ amazonaws/bin/allocate-tomcats.sh
@@ -29,3 +29,45 @@ $AWS_ELB_HOME/bin/elb-create-lb todd-lb --availability-zones us-east-1a --listen
 
 echo query elb 
 $AWS_ELB_HOME/bin/elb-describe-lbs
+elb=`$AWS_ELB_HOME/bin/elb-describe-lbs | awk '{print $3}'`
+
+
+
+$AWS_ELB_HOME/bin/elb-configure-healthcheck todd-lb --interval 30 --unhealthy-threshold 2 --healthy-threshold 2 --timeout 3  --target "http:8080/hostname.txt" --headers       
+
+echo setup hostname.txt
+names=`ec2-describe-instances  | grep ec2 | cut -d\| -f6`
+for i in $names
+do
+    echo setting hostname $i
+    echo $i > /tmp/hostname.txt
+    scp root@$i /tmp/hostname.txt /usr/share/tomcat5/webapps/ROOT/hostname.txt
+    echo GETing hostname $i
+    GET http://$i:8080/hostname.txt
+done
+
+
+echo setup hostname.txt
+instances=`ec2-describe-instances  | grep ec2 | cut -d\| -f2`
+for i in $instances
+do
+    $AWS_ELB_HOME/bin/elb-register-instances-with-lb  todd-lb --instances $i
+done
+
+$AWS_ELB_HOME/bin/elb-describe-instance-health  todd-lb
+
+
+echo illustrate round-robin
+for i in {1..5}
+do
+  GET http://$elb/hostname.txt
+done
+
+
+
+
+
+# based on 
+#   http://java.dzone.com/articles/clustering-java-web
+# and 
+#   http://paulstamatiou.com/how-to-getting-started-with-amazon-ec2
