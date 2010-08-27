c38ab80e634f1b28555cba56034efa879e3781aa
Todd Hodes <todd@locationlabs.com>
2010-08-27 03:05:11 +0000

    add - getHistory samples - getAssets samples - location failure result
    fix up naming of location results:
    - loc.result-pending.result
    - loc.success.result
    - loc.fail.result
    
    
    
    
    
    git-svn-id: svn+ssh://svn/wm/users/todd@130087 33ded623-5efc-0310-b085-d7f78f97f785

---
+++ MeeGo/finder-json-api/getAssets.sh
@@ -0,0 +1,2 @@
+cat getAssets.json | POST -sS "http://meego.location-labs.com:8080/finder-att-family/finderApiJson.svc?method=getAssets&service=account&v=1"
+
---
+++ MeeGo/finder-json-api/getHistory.sh
@@ -0,0 +1,2 @@
+cat getHistory.json | POST -sS "http://meego.location-labs.com:8080/finder-att-family/finderApiJson.svc?method=getLocationHistory&service=history&v=1"
+
