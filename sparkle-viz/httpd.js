757938cf93eb2cd640b076b6e128a18bb99051d4
Todd Hodes <todd@locationlabs.com>
2011-04-06 23:10:00 +0000

    bug:nobug
    playing w/ node.js for potential sparkle viz server.  adding files:
    
    - httd.js -- simple http server listening on my tunnel port (8421)
    
    plus
    - httpsd.js -- untested sample of https support
    - samples -- some add'l general usage samples
    
    
    
    git-svn-id: svn+ssh://svn/wm/users/todd@144456 33ded623-5efc-0310-b085-d7f78f97f785

---
+++ sparkle-viz/httpd.js
@@ -0,0 +1,48 @@
+
+var sys = require("sys");
+puts = require("sys").puts;
+
+var reqs = 0;
+var posts = 0;
+
+function log(arg) {
+  puts(new Date() + ": " + arg);
+}
+
+function stat() {
+  log("have recv'd " + reqs + " requests, " + posts + " w/ post bodys");
+}
+setInterval(stat, 15000);
+
+process.addListener("SIGINT",
+  function () {
+    stat();
+    log("goodbye");
+    process.exit(0)
+  });
+
+
+var http = require('http');
+var s = http.createServer(function (req, res) {
+  req.on('data', function (chunk) {
+    console.log('BODY: ' + chunk);
+    posts++;
+  });
+  reqs++;
+  console.log(req.headers);
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+});
+//s.listen(8421, "127.0.0.1");
+s.listen(8421);
+log('Server running at http://'
+    + s.address().address + ':' + s.address().port);
+
+var util = require('util');
+
+//log("server info: " + s.address());
+//console.log(util.inspect(s.address(), true, null));
+//for(prop in s) log("server " + prop + ": " + s[prop]);
+
+
+
---
+++ sparkle-viz/httpsd.js
@@ -0,0 +1,27 @@
+
+const crypto = require('crypto'),
+      fs = require("fs"),
+      http = require("http");
+
+var privateKey = fs.readFileSync('privatekey.pem').toString();
+var certificate = fs.readFileSync('certificate.pem').toString();
+
+var credentials = crypto.createCredentials({key: privateKey, cert: certificate});
+
+var handler = function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+};
+
+var server = http.createServer();
+server.setSecure(credentials);
+server.addListener("request", handler);
+server.listen(8000);
+
+/*
+openssl genrsa -out privatekey.pem 1024 
+openssl req -new -key privatekey.pem -out certrequest.csr 
+openssl x509 -req -in certrequest.csr -signkey privatekey.pem -out certificate.pem
+
+http://www.silassewell.com/blog/2010/06/03/node-js-https-ssl-server-example/
+ */
