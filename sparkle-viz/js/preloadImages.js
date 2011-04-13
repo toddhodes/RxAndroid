c9d9ede3ebcbd559c617c607f15a21c80e51fc79
Todd Hodes <todd@locationlabs.com>
2011-04-13 03:20:33 +0000

    fix undefined vars
    git-svn-id: svn+ssh://svn/wm/users/todd@144904 33ded623-5efc-0310-b085-d7f78f97f785

--- sparkle-viz/js/preloadImages.js
+++ sparkle-viz/js/preloadImages.js
@@ -2,8 +2,7 @@
 //preload images
 var pauseImage = new Image(214,54);
 var mainImage = new Image(214,54);
-var pauseOverImage = new Image(214,54);
 
 //src for preload images
-pauseButtonImg.src = "../images/ui/pauseButton.png";
-mainButtonImg.src = "../images/ui/mainButton.png";
+pauseImage.src = "../images/ui/pauseButton.png";
+mainImage.src = "../images/ui/mainButton.png";
