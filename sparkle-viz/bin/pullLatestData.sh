ce645415d7a491a7fcf5d608194b29ddbe965c79
Todd Hodes <todd@locationlabs.com>
2011-04-19 20:47:53 +0000

    bug:nobug
    add local cached data copies, accessible for development,
    and scripts for pushing a pulling same.
    
    
    
    git-svn-id: svn+ssh://svn/wm/users/todd@145324 33ded623-5efc-0310-b085-d7f78f97f785

---
+++ sparkle-viz/bin/pullLatestData.sh
@@ -0,0 +1,7 @@
+
+for i in 5105551212 5105551213 5106107008 5103326150 5109184126 
+do
+	GET http://sparkledemo.locationlabs.com/finder-att-family/location_feed/$i/playback.svc \
+		| python -mjson.tool \
+		> data/$i.json
+done
