32df5b88cc117d1cf7d1e0415f0927d59a55a87f
Todd Hodes <todd@locationlabs.com>
2011-05-02 23:16:23 +0000

    bug:33380
    - add formatCSV.node.js for converting from data/<MDN>.json formatted
      data to CSV data.  
    - update data/ to latest via pull
    - add current generated CSV as data/locdata.csv
    - rm fake numbers: 5105551212 5105551213
    
    
    
    git-svn-id: svn+ssh://svn/wm/users/todd@145996 33ded623-5efc-0310-b085-d7f78f97f785

--- sparkle-viz/bin/pullLatestData.sh
+++ sparkle-viz/bin/pullLatestData.sh
@@ -1,5 +1,5 @@
 
-for i in 5105551212 5105551213 5106107008 5103326150 5109184126 
+for i in 5105551212 5105551213 5106107008 5103326150 5109184126 5109184123
 do
 	GET http://sparkledemo.locationlabs.com/finder-att-family/location_feed/$i/playback.svc \
 		| python -mjson.tool \
