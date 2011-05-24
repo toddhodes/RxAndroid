c5eb7565ccfddef34d19d27fd9a1a02f3ec69d58
Todd Hodes <todd@locationlabs.com>
2011-05-24 23:27:48 +0000

    an irc logging bot
    git-svn-id: svn+ssh://svn/wm/users/todd@147279 33ded623-5efc-0310-b085-d7f78f97f785

---
+++ LogBot/html/footer.inc.php
@@ -0,0 +1,20 @@
+<p>
+ These logs were automatically created by user
+<b><?php echo($nick); ?></b> 
+on
+ <a href="irc://<?php echo($server . "/" . substr($channel, 1)); ?>"><?php echo($server); ?></a>
+ using <b>llLogBot</b>, 
+based on code from <a href="http://www.jibble.org/logbot/">jibble.org/logbot</a>.
+</p>
+<p>
+  source and install:  <b>bugz:/opt/LogBot</b>
+</p>
+
+ <p>
+     <a href="../">All Channels</a>
+ </p>
+
+
+</body>
+
+</html>
